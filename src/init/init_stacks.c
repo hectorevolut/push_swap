06a3309bf56ed06461b854927fa2c18292d86e95
Héctor Calderón Hervás <hector@evolutplatform.com>
2026-04-24 10:22:26 +0200

    Implement stack initialization function
    Initialize stacks with given capacity and allocate memory.

--- src/init/init_stacks.c
+++ src/init/init_stacks.c
@@ -21,3 +21,27 @@ void free_stacks(t_ps *ps)
     ps->size_b = 0;
     ps->capacity = 0;
 }
+
+int init_stacks(t_ps *ps, int capacity)
+{
+    if (!ps)
+        return (0);
+    if (capacity <= 0)
+        return (0);
+    ps->capacity = capacity;
+    ps->a = malloc(capacity * sizeof(int));
+    if (!ps->a)
+    {
+        return (0);
+    }
+    ps->b = malloc(capacity * sizeof(int));
+    if (!ps->b)
+    {
+        free(ps->a);
+        ps->a = NULL;
+        return (0);
+    }
+    ps->size_a = 0;
+    ps->size_b = 0;
+    return (1);
+}
