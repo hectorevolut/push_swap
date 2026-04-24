dd5491197ecf49e7032bde25ed0fc14546fea0cb
Héctor Calderón Hervás <hector@evolutplatform.com>
2026-04-24 11:11:35 +0200

    Refactor swap functions and add ss functionality
    Refactored sa and sb functions to use a new swap helper function. Added ss function to swap both stacks simultaneously.

--- src/operations/swap.c
+++ src/operations/swap.c
@@ -1,15 +1,21 @@
 # include "push_swap.c"
 
-void	sa(t_ps *ps)
-{
-	int	temp;
+void swap(int *a, int *b) {
+    int temp;
 
-	if (!ps || !ps->a || ps->size_a < 2)
-		return ;
-	temp = ps->a[0];
-	ps->a[0] = ps->a[1];
-	ps->a[1] = temp;
-	write(1, "sa\n", 3);
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void sa(t_ps *ps)
+{
+    int temp;
+
+    if (!ps || !ps->a || ps->size_a < 2)
+        return;
+    swap(&ps->a[0], &ps->a[1]);
+    write(1, "sa\n", 3);
 }
 
 void sb(t_ps *ps)
@@ -18,8 +24,21 @@ void sb(t_ps *ps)
 
     if (!ps || !ps->b || ps->size_b < 2)
         return;
-    temp = ps->b[0];
-    ps->b[0] = ps->b[1];
-    ps->b[1] = temp;
+    swap(&ps->b[0], &ps->b[1]);
     write(1, "sb\n", 3);
+}
+
+void ss(t_ps *ps) {
+    int temp;
+
+    if (!ps)
+        return;
+    else {
+        if (!ps->a || ps->size_a < 2 || !ps->b || ps->size_b < 2)
+            return;
+        else 
+            swap(&ps->a[0], &ps->a[1]);
+            swap(&ps->b[0], &ps->b[1]);
+    }
+    write (1, "ss\n", 3);
 }
