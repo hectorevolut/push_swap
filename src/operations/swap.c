50324576a52bea11f35049473b742bfd10ac7a8a
Héctor Calderón Hervás <hector@evolutplatform.com>
2026-04-24 10:54:27 +0200

    Add sb function for swapping top two elements

--- src/operations/swap.c
+++ src/operations/swap.c
@@ -12,3 +12,14 @@ void	sa(t_ps *ps)
 	write(1, "sa\n", 3);
 }
 
+void sb(t_ps *ps)
+{
+    int temp;
+
+    if (!ps || !ps->b || ps->size_b < 2)
+        return;
+    temp = ps->b[0];
+    ps->b[0] = ps->b[1];
+    ps->b[1] = temp;
+    write(1, "sb\n", 3);
+}
