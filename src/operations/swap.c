5d28653004cef03198d7e363de3f99f4c969b1ef
Héctor Calderón Hervás <hector@evolutplatform.com>
2026-04-24 11:23:37 +0200

    Refactor swap functions to use swap_values
    Refactor swap functions to use swap_values for clarity and correctness.

--- src/operations/swap.c
+++ src/operations/swap.c
@@ -1,44 +1,45 @@
-# include "push_swap.c"
-
-void swap(int *a, int *b) {
-    int temp;
-
-    temp = a;
-    a = b;
-    b = temp;
-}
-
-void sa(t_ps *ps)
+static void	swap_values(int *x, int *y)
 {
-    int temp;
+	int	temp;
 
-    if (!ps || !ps->a || ps->size_a < 2)
-        return;
-    swap(&ps->a[0], &ps->a[1]);
-    write(1, "sa\n", 3);
+	temp = *x;
+	*x = *y;
+	*y = temp;
 }
 
-void sb(t_ps *ps)
+void	sa(t_ps *ps)
 {
-    int temp;
-
-    if (!ps || !ps->b || ps->size_b < 2)
-        return;
-    swap(&ps->b[0], &ps->b[1]);
-    write(1, "sb\n", 3);
+	if (!ps || !ps->a || ps->size_a < 2)
+		return ;
+	swap_values(&ps->a[0], &ps->a[1]);
+	write(1, "sa\n", 3);
 }
 
-void ss(t_ps *ps) {
-    int temp;
+void	sb(t_ps *ps)
+{
+	if (!ps || !ps->b || ps->size_b < 2)
+		return ;
+	swap_values(&ps->b[0], &ps->b[1]);
+	write(1, "sb\n", 3);
+}
 
-    if (!ps)
-        return;
-    else {
-        if (!ps->a || ps->size_a < 2 || !ps->b || ps->size_b < 2)
-            return;
-        else 
-            swap(&ps->a[0], &ps->a[1]);
-            swap(&ps->b[0], &ps->b[1]);
-    }
-    write (1, "ss\n", 3);
+void	ss(t_ps *ps)
+{
+	int	done;
+
+	if (!ps)
+		return ;
+	done = 0;
+	if (ps->a && ps->size_a >= 2)
+	{
+		swap_values(&ps->a[0], &ps->a[1]);
+		done = 1;
+	}
+	if (ps->b && ps->size_b >= 2)
+	{
+		swap_values(&ps->b[0], &ps->b[1]);
+		done = 1;
+	}
+	if (done)
+		write(1, "ss\n", 3);
 }
